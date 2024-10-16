7da9b536ea2090ec17b4a1788503c5334a2d0b78
Walid Boudabbous <w.boudabbous@dudoxx.com>
2024-10-17 01:43:25 +0200

    Task 1: Create base folder structure and placeholder files

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Dudoxx Refactored',
+    'version': '16.0.1.0.0',
+    'category': 'Custom',
+    'summary': 'Refactored module for Dudoxx',
+    'author': 'Walid Boudabbous, Dudoxx UG',
+    'website': 'https://www.dudoxx.com',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [
+        'security/access_rules.xml',
+        'views/main_menu.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
